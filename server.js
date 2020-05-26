527968bf204e370233e86a7e0408aa3b3f7cb9e7
Francis H <francishodianto@Franciss-MacBook-Air.local>
2020-05-27 01:45:08 +0800

    Allow Frontend to access using cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const cors = require("cors");
 
 const users = require("./routes/api/users");
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({
     extended: false
 }));
 
+app.use(cors);
 
 // Gets the URI of the MongoDB database used by app
 const db = require("./config/keys").mongoURI;
